9dc14de35aabfaa519423379da1e8be821e1b07a
rop borney <birnxy@gmail.com>
2025-03-14 08:03:40 -0700

    kh

--- monolisa/default.nix
+++ monolisa/default.nix
@@ -5,7 +5,7 @@ stdenv.mkDerivation {
   version = "2.015";
   src = fetchzip {
     url = "https://fourdaycreep.com/s/rBnnEYSWCAeeE6f/download/monolisa.zip";
-    hash = "sha256-H+DhKCNvcA6BL7BCA0klGwjRVb9CLGFxmYEthFExGfI=";
+    hash = "sha256-Vz78eXMRlft9ZZHD0L0Qh6u2GB7c4E6uy+Dh77uFKG0=";
     stripRoot = false;
   };
 
